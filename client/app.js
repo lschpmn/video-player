c07e3ffe2145444e8364bb90938578e1df751e83
lschpmn <luis@lmc-productions.com>
2017-07-29 20:35:00 -0500

    updated layout

--- client/app.js
+++ client/app.js
@@ -1,10 +1,11 @@
 'use strict';
 
 import React, { Component } from 'react';
+import { Glyphicon } from 'react-bootstrap';
 
 export default class App extends Component {
   render() {
-    return <div style={{display: 'flex', flexWrap: 'wrap', height: '100%'}}>
+    return <div style={styles.parent}>
       <div style={{width: '50%'}}>
         <h4>Playlist</h4>
       </div>
@@ -13,9 +14,45 @@ export default class App extends Component {
         <h4>Dropzone</h4>
       </div>
 
-      <div style={{width: '100%'}}>
-        <h4>Controls</h4>
+      <div style={styles.controlContainer}>
+        <div style={styles.controlItems}>
+          previous
+        </div>
+
+        <div style={styles.controlItems}>
+          skip back
+        </div>
+
+        <div style={{...styles.controlItems, fontSize: 50}}>
+          <Glyphicon glyph='play' />
+        </div>
+
+        <div style={styles.controlItems}>
+          skip forward
+        </div>
+
+        <div style={styles.controlItems}>
+          next
+        </div>
       </div>
     </div>;
   }
 }
+
+const styles = {
+  parent: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    height: '100%',
+  },
+
+  controlContainer: {
+    display: 'flex',
+    width: '100%',
+  },
+
+  controlItems: {
+    fontSize: 30,
+    width: '20%',
+  },
+};
