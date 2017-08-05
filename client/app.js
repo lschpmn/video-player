b2b80f8dd7536679e16db052c549ada1b31217e4
lschpmn <luis@lmc-productions.com>
2017-08-05 11:33:31 -0500

    setting up basic state

--- client/app.js
+++ client/app.js
@@ -4,35 +4,43 @@ import React, { Component } from 'react';
 import { Glyphicon } from 'react-bootstrap';
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      media: [{}],
+      play: false,
+    };
+  }
+
   render() {
     return <div style={styles.parent}>
-      <div style={{width: '50%'}}>
+      <div style={{height: '75%', width: '50%'}}>
         <h4>Playlist</h4>
       </div>
 
-      <div style={{width: '50%'}}>
+      <div style={{height: '75%', width: '50%'}}>
         <h4>Dropzone</h4>
       </div>
 
       <div style={styles.controlContainer}>
         <div style={styles.controlItems}>
-          previous
+          <Glyphicon glyph='fast-backward' style={styles.icon} />
         </div>
 
         <div style={styles.controlItems}>
-          skip back
+          <Glyphicon glyph='backward' style={styles.icon} />
         </div>
 
-        <div style={{...styles.controlItems, fontSize: 50}}>
-          <Glyphicon glyph='play' />
-        </div>
-
-        <div style={styles.controlItems}>
-          skip forward
+        <div style={styles.controlItems} onClick={() => this.setState({ play: !this.state.play })}>
+          <Glyphicon glyph={this.state.play ? 'play' : 'pause'} style={styles.icon} />
         </div>
 
         <div style={styles.controlItems}>
-          next
+          <Glyphicon glyph='forward' style={styles.icon} />
+        </div>
+
+        <div style={styles.controlItems}>
+          <Glyphicon glyph='fast-forward' style={styles.icon} />
         </div>
       </div>
     </div>;
@@ -40,19 +48,26 @@ export default class App extends Component {
 }
 
 const styles = {
-  parent: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    height: '100%',
-  },
-
   controlContainer: {
     display: 'flex',
+    height: '25%',
     width: '100%',
   },
 
   controlItems: {
-    fontSize: 30,
+    fontSize: 50,
     width: '20%',
+  },
+
+  icon: {
+    cursor: 'pointer',
+    display: 'block',
+    textAlign: 'center',
+  },
+
+  parent: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    height: '100%',
   },
 };
