b500dda9af3066832e2ac36dcaae93dbf2dfa94d
lschpmn <luis@lmc-productions.com>
2018-07-10 00:27:51 -0500

    Actually found chromecast!

--- server/Chromecast.ts
+++ server/Chromecast.ts
@@ -1,7 +1,36 @@
+const Client = require('castv2-client').Client;
+const mdns = require('multicast-dns')();
+const ssdp = require('node-ssdp').Client;
 
+const chromecastInfo = {
+  serviceName: '_googlecast._tcp.local',
+  serviceType: 'PTR',
+};
 
 export default class Chromecast {
-  constructor() {
+  private readonly client: any;
+  private readonly ssdp: any;
 
+  constructor() {
+    this.client = new Client();
+    this.ssdp = new ssdp();
+
+    mdns.on('response', this.response);
+    this.connect();
+  }
+
+  private connect() {
+    mdns.query({
+      questions:[{
+        name: chromecastInfo.serviceName,
+        type: chromecastInfo.serviceType,
+      }]
+    });
+  }
+
+  private response = response => {
+    console.log(response);
   }
 }
+
+const chromecast = new Chromecast();
