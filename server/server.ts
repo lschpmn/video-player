8106d0ef447de6c28b0ea043ae128037d92367f4
lschpmn <luis@lmc-productions.com>
2018-07-23 21:28:40 -0500

    Added method to get drives

--- server/server.ts
+++ server/server.ts
@@ -1,14 +1,26 @@
+import { exec } from 'child_process';
 import { listAsync as list } from 'fs-jetpack';
 import { Request, Response } from 'express';
 
 const cors = require('cors');
 const express = require('express');
 
+const PREFIX = '/api/files';
 const app = express();
 
 app.use(cors());
 
-app.get('/api/files/list/:path', async (req: Request, res: Response) => {
+app.get(PREFIX + '/get-drives', (req: Request, res: Response) => {
+  exec(' wmic logicaldisk get caption', (err, stdout) => {
+    if (err) res.status(500).send(err);
+
+    const drives = stdout.match(/\w:/g);
+    console.log(drives);
+    res.send(drives);
+  });
+});
+
+app.get(PREFIX + '/list/:path', async (req: Request, res: Response) => {
   const path = decodeURIComponent(req.params.path);
   const files = await list(path);
 
