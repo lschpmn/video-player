c9688c42d4f5045410e0afb44d3c67fc37fb2e36
lschpmn <luis@lmc-productions.com>
2018-07-27 17:31:02 -0500

    Converted FileUpload component to typescript

---
+++ client/components/FileUpload.tsx
@@ -0,0 +1,39 @@
+'use strict';
+
+import * as React from 'react';
+
+type Props = {
+  play: (path: string) => void,
+};
+
+export default class FileUpload extends React.Component<Props> {
+  drop = event => {
+    event.preventDefault();
+    
+    const { dataTransfer } = event;
+    const files = [];
+    
+    for (let file of dataTransfer.files) {
+      files.push(file);
+    }
+    
+    console.log(files);
+    this.props.play(files[0].path);
+  };
+  
+  eventNoop = (event) => {
+    event.preventDefault();
+  };
+  
+  render() {
+    return <div
+      id='dropzone'
+      style={{ height: '100%', width: '100%' }}
+      onDrop={this.drop}
+      onDragOver={this.eventNoop}
+      onDragEnd={this.eventNoop}
+    >
+      {this.props.children}
+    </div>;
+  }
+}
