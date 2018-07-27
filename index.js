8048c6d478a4019a8108df1280374cad97295ea2
lschpmn <luis@lmc-productions.com>
2018-07-27 17:31:23 -0500

    Better code style for electron index file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const {app, BrowserWindow} = require('electron');
+const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const url = require('url');
 
@@ -8,9 +8,9 @@ const url = require('url');
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600});
+  win = new BrowserWindow({ width: 800, height: 600 });
 
   // and load the index.html of the app.
   /*win.loadURL(url.format({
@@ -28,8 +28,8 @@ function createWindow () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    win = null
-  })
+    win = null;
+  });
 }
 
 // This method will be called when Electron has finished
@@ -39,5 +39,5 @@ app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  app.quit()
+  app.quit();
 });
