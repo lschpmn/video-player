ff3afb418e993c5b5e42933495f7c0aa18e16c45
lschpmn <luis@lmc-productions.com>
2018-07-27 17:32:15 -0500

    Made source.sh more portable, since what other directory would I be launching stuff from?

--- source.sh
+++ source.sh
@@ -1,2 +1,2 @@
-alias ts-node="/c/git/video-player/node_modules/.bin/ts-node";
-alias server-watch="nodemon -w ./server -e ts -x /c/git/video-player/node_modules/.bin/ts-node server/server.ts < /dev/null"
+alias ts-node="./node_modules/.bin/ts-node";
+alias server-watch="nodemon -w ./server -e ts -x ./node_modules/.bin/ts-node server/server.ts < /dev/null"
