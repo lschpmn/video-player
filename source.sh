d29cd4b209a5a4232205b928b15fe8f6f6d990c4
lschpmn <luis@lmc-productions.com>
2018-08-02 18:49:26 -0500

    Updated source.sh to new standard

--- source.sh
+++ source.sh
@@ -1,2 +1,5 @@
-alias ts-node="./node_modules/.bin/ts-node";
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+PATH=$PATH:$DIR/node_modules/.bin/
 alias server-watch="nodemon -w ./server -e ts -x ./node_modules/.bin/ts-node server/server.ts < /dev/null"
