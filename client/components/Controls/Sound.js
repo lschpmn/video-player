5cd7902abec884c47c4976f677478f973ac72e01
lschpmn <luis@lmc-productions.com>
2018-08-04 11:35:22 -0500

    Changed import

--- client/components/Controls/Sound.js
+++ client/components/Controls/Sound.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import React, { Component } from 'react';
-import { Slider } from 'material-ui';
+import Slider from 'material-ui/Slider';
 
 export default class Sound extends Component {
   constructor(props) {
