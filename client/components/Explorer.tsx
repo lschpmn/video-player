d15acb4c9c1ade7859ed286051989584fde1d43f
lschpmn <luis@lmc-productions.com>
2018-08-04 22:14:46 -0500

    Added Explorer component

---
+++ client/components/Explorer.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { connect } from 'react-redux';
+
+class Explorer extends React.Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default connect(
+  state => ({
+    
+  }),
+  {}
+)(Explorer);
