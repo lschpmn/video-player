474c2bff2fb933ea8f7d46e94afc0356d75a7e95
lschpmn <luis@lmc-productions.com>
2018-08-05 21:36:26 -0500

    Setting up client side Chromecast solution

---
+++ client/lib/Chromecast.ts
@@ -0,0 +1,2 @@
+
+export function play(url: string) {}
--- client/lib/file-actions.ts
+++ client/lib/file-actions.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { join } from 'path';
 
-const HOST = 'http://localhost:3001/api/files';
+const HOST = 'http://localhost:3000/api/files';
 export const GET_DRIVES = 'file/GET_DRIVES';
 export const GET_FILES = 'file/GET_FILES';
 export const INSPECT_FILE = 'INSPECT_FILE';
