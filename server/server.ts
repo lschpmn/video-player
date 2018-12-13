2959649ed00eca29470adc5a723c4e2b083cdb24
lschpmn <luis@lmc-productions.com>
2018-12-12 22:01:40 -0600

    Added websocket

--- server/server.ts
+++ server/server.ts
@@ -14,3 +14,25 @@ app.use('/api/cast', ChromecastRouter);
 app.use('/api/files', FilesRouter);
 
 app.listen(3000, () => console.log('server running on port 3000'));
+
+const WebSocketClient = require('websocket').client;
+
+const client = new WebSocketClient();
+
+client.on('connect', function(connection) {
+  console.log('WebSocket Client Connected');
+  connection.on('error', function(error) {
+    console.log("Connection Error: " + error.toString());
+  });
+  connection.on('close', function() {
+    console.log('echo-protocol Connection Closed');
+  });
+  connection.on('message', function(message) {
+    if (message.type === 'utf8') {
+      console.log("Received: '" + message.utf8Data + "'");
+    }
+  });
+});
+
+console.log('trying to connect');
+client.connect('ws://192.168.1.107:8009/');
