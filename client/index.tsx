f14a4d08ff429441023e9f886ee2e8b16cb81e65
lschpmn <luis@lmc-productions.com>
2019-11-04 23:43:09 -0600

    Cleanup

--- client/index.tsx
+++ client/index.tsx
@@ -3,14 +3,13 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
-import thunk from 'redux-thunk';
 import App from './App';
 import reducers from './lib/reducers';
 import socketMiddleware from './lib/socketMiddleware';
 
 import './index.html';
 
-const store = createStore(reducers, applyMiddleware(thunk, socketMiddleware));
+const store = createStore(reducers, applyMiddleware(socketMiddleware));
 store.subscribe(() => console.log(store.getState()));
 
 render((
