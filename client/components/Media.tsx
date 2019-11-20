026b8eeb14ec04e4831bb45d10ef924d9edfcbd3
lschpmn <luis@lmc-productions.com>
2019-11-20 00:01:34 -0600

    WIP Client side done for "play url" button

--- client/components/Media.tsx
+++ client/components/Media.tsx
@@ -1,3 +1,5 @@
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 import CastIcon from '@material-ui/icons/Cast';
 import CastConnectedIcon from '@material-ui/icons/CastConnected';
 import * as React from 'react';
@@ -6,18 +8,28 @@ import { colors } from '../../constants';
 import { connect as connectAction, getChromecasts } from '../lib/player-actions';
 import { ChromecastStoreState, ReducerState } from '../types';
 
+type State = {
+  showUrlField: boolean,
+  url: string,
+};
+
 type Props = {
   chromecastStore: ChromecastStoreState,
   connectAction: typeof connectAction,
   getChromecasts: typeof getChromecasts,
 };
 
-class Media extends React.Component<Props> {
+class Media extends React.Component<Props, State> {
+  state = {
+    showUrlField: false,
+    url: '',
+  };
+
   componentDidMount() {
     this.props.getChromecasts();
   }
 
-  componentDidUpdate(prevProps: Props) {
+  componentDidUpdate(prevProps: Props, prevState: State) {
     const { chromecastStore: prevChromecastStore } = prevProps;
     const { chromecastStore } = this.props;
 
@@ -25,21 +37,57 @@ class Media extends React.Component<Props> {
       || prevChromecastStore.loading && !chromecastStore.loading && !chromecastStore.isConnected) {
       setTimeout(() => this.props.connectAction(chromecastStore.chromecasts[0].host), 500);
     }
+
+    if (!prevState.showUrlField && this.state.showUrlField) {
+      document.addEventListener('keydown', this.onEnter);
+    } else if (prevState.showUrlField && !this.state.showUrlField) {
+      document.removeEventListener('keydown', this.onEnter);
+    }
   }
+
+  onEnter = ({ key }) => {
+    if (key === 'Enter') {
+      console.log(this.state.url);
+      this.setState({
+        showUrlField: false,
+        url: '',
+      });
+    }
+  };
 
   render() {
     const { chromecastStore } = this.props;
     const name = chromecastStore.chromecasts[0] && chromecastStore.chromecasts[0].name;
 
-    return <div>
+    return <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
       {chromecastStore.isConnected
         ? chromecastStore.mediaStatus
-          ? <CastConnectedIcon style={{ ...styles.icon, color: colors.blue }} />
-          : <CastIcon style={{ ...styles.icon, color: colors.green }} />
-        : <CastIcon style={{ ...styles.icon, color: colors.red }} />}
+          ? <CastConnectedIcon style={{ ...styles.icon, color: colors.blue }}/>
+          : <CastIcon style={{ ...styles.icon, color: colors.green }}/>
+        : <CastIcon style={{ ...styles.icon, color: colors.red }}/>}
       <div style={styles.name}>{name}</div>
       <div style={styles.name}>{chromecastStore.mediaStatus?.title}</div>
       <div style={styles.name}>{chromecastStore.mediaStatus?.subtitle}</div>
+
+      <div style={{ flex: 1 }}/>
+
+      {this.state.showUrlField
+        ? <TextField
+            label="URL"
+            onBlur={() => this.setState({ showUrlField: false })}
+            onChange={e => this.setState({ url: e.target.value })}
+            style={styles.urlInput}
+            variant="filled"
+            value={this.state.url}
+        />
+        : <Button
+            onMouseDown={() => this.setState({ showUrlField: true })}
+            style={styles.urlInput}
+            variant="contained"
+        >
+          Add URL
+        </Button>
+      }
     </div>;
   }
 }
@@ -51,7 +99,7 @@ export default connect(
   {
     connectAction,
     getChromecasts,
-  }
+  },
 )(Media);
 
 const styles = {
@@ -69,5 +117,9 @@ const styles = {
     margin: '0 auto',
     padding: '0.5rem 0',
     width: '2rem',
+  } as React.CSSProperties,
+  urlInput: {
+    height: '3rem',
+    margin: '2rem 0',
   } as React.CSSProperties,
 };
