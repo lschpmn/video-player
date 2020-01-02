da4840bc09f9279be332e5188e1fd15420d94ca8
lschpmn <luis@lmc-productions.com>
2020-01-02 16:39:23 -0600

    Setting up better navigation for explorer window

--- client/components/Explorer/index.tsx
+++ client/components/Explorer/index.tsx
@@ -2,13 +2,15 @@ import { join } from 'path';
 import * as React from 'react';
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { requestDrives, requestFileItems } from '../../lib/utils';
+import { setCurrentLocation } from '../../lib/file-actions';
+import { requestDrives, requestFileItems, useAction } from '../../lib/utils';
 import { FileStructure, ReducerState } from '../../types';
 import ExplorerItem from './ExplorerItem';
 
 const Explorer = () => {
   const [files, setFiles] = useState(null as FileStructure | null);
   const currentLocation = useSelector((state: ReducerState) => state.explorer.currentLocation);
+  const setCurrentLocationAction = useAction(setCurrentLocation);
 
   useEffect(() => {
     const request = currentLocation.length === 0
@@ -21,27 +23,43 @@ const Explorer = () => {
   }, [currentLocation]);
 
   return <div style={styles.container}>
-    {files && Object
-      .entries(files)
-      .sort(([aName, aItem], [bName, bItem]) => {
-        if (aItem.type === 'dir' && bItem.type === 'dir') {
-          return aName.localeCompare(bName);
-        } else if (aItem.type === 'dir' && bItem.type !== 'dir') {
-          return -1;
-        } else if (aItem.type !== 'dir' && bItem.type === 'dir') {
-          return 1;
-        }
-
-        return aName.localeCompare(bName);
-      })
-      .map(([name, item]) =>
-        <ExplorerItem
-          currentLocation={currentLocation}
-          drive={item}
-          key={join(...currentLocation, name)}
-          name={name}
-        />,
+    <div style={{ display: 'flex' }}>
+      <h3 style={styles.explorerItem} onMouseDown={() => setCurrentLocationAction([])}>
+        /
+      </h3>
+      {currentLocation.map((loc, i) =>
+        <h3
+          key={[...currentLocation.slice(0, i + 1), loc].join('/')}
+          onMouseDown={() => setCurrentLocationAction(currentLocation.slice(0, i + 1))}
+          style={styles.explorerItem}
+        >
+          {loc}/
+        </h3>
       )}
+    </div>
+    <div style={styles.itemContainer}>
+      {files && Object
+        .entries(files)
+        .sort(([aName, aItem], [bName, bItem]) => {
+          if (aItem.type === 'dir' && bItem.type === 'dir') {
+            return aName.localeCompare(bName);
+          } else if (aItem.type === 'dir' && bItem.type !== 'dir') {
+            return -1;
+          } else if (aItem.type !== 'dir' && bItem.type === 'dir') {
+            return 1;
+          }
+
+          return aName.localeCompare(bName);
+        })
+        .map(([name, item]) =>
+          <ExplorerItem
+            currentLocation={currentLocation}
+            drive={item}
+            key={join(...currentLocation, name)}
+            name={name}
+          />
+        )}
+    </div>
   </div>;
 };
 
@@ -49,11 +67,20 @@ export default Explorer;
 
 const styles = {
   container: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%',
+  } as React.CSSProperties,
+  explorerItem: {
+    cursor: 'pointer',
+    margin: '4px 0',
+    paddingLeft: '0.5rem',
+  } as React.CSSProperties,
+  itemContainer: {
     alignContent: 'start',
     display: 'flex',
     flexDirection: 'row',
     flexWrap: 'wrap',
-    height: '100%',
     margin: '0 0.5rem',
     overflow: 'auto',
   } as React.CSSProperties,
