415f443f9afd447f3dbdaa66e4555642f13cde50
lschpmn <luis@lmc-productions.com>
2020-11-09 22:30:57 -0600

    WIP Adding Modal

--- client/components/TopBar.tsx
+++ client/components/TopBar.tsx
@@ -1,22 +1,47 @@
 import Modal from '@material-ui/core/Modal';
 import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import Input from '@material-ui/core/Input';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import useTheme from '@material-ui/core/styles/useTheme';
 import AddIcon from '@material-ui/icons/Add';
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import * as React from 'react';
 import { hot } from 'react-hot-loader/root';
+import { launch } from '../lib/player-actions';
+import { useAction } from '../lib/utils';
 
 const TopBar = () => {
   const classes = useStyles({});
   const theme = useTheme();
   const [open, setOpen] = useState(false);
-  console.log(theme.palette)
+  const [url, setUrl] = useState("");
+  const launchAction = useAction(launch);
+
+  const onEnter = useCallback(({ key }) => {
+    if (key === 'Enter') {
+      console.log('enter hit');
+      launchAction(url, true);
+      console.log('launch');
+      setOpen(false);
+      setUrl('');
+    }
+  }, [url]);
+
+  useEffect(() => {
+    if (open) {
+      document.addEventListener('keydown', onEnter);
+      return () => document.removeEventListener('keydown', onEnter);
+    }
+  }, [open, onEnter]);
+  console.log(`open ${open}`);
 
   return <div className={classes.container}>
-    <IconButton onClick={() => setOpen(true)}>
+    <IconButton onClick={() => {
+      console.log('setOpen true')
+      setOpen(true);
+    }}>
       <AddIcon style={{ color: 'white' }}/>
     </IconButton>
     <Modal open={open} onBackdropClick={() => setOpen(false)} style={{
@@ -33,7 +58,20 @@ const TopBar = () => {
         outline: 'none',
         padding: '2rem',
       }}>
-        <Input />
+        <Input
+          onChange={e => setUrl(e.target.value)}
+          value={url}
+        />
+        <div style={{ height: '1rem' }} />
+        <Button
+          color='secondary'
+          fullWidth
+          onClick={onEnter}
+          style={{ color: 'white' }}
+          variant='contained'
+        >
+          Add URL
+        </Button>
       </Paper>
     </Modal>
   </div>;
